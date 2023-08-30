275b94d9ee8c811f66452aa8934bff620bfa59a6
Gulshan Kumar Prasad <gulshankumarprasad04@gmail.com>
2023-08-30 13:19:20 +0530

    first commit

---
+++ Automating twitter/README.md
@@ -0,0 +1,14 @@
+# Automation for tweeting
+
+This is not truly a automation, it is a keyboard and mouse automation for tweeting
+
+To use the `automattingTwitter.py` for yourself
+
+* keep few condition need to be met
+  * You are already logged in
+  * Your screen size should be 1920 x 1080
+  * Your browser view size should be 100% to adjust the view to `ctrl` + `+` or `-`
+
+How you can do it
+  * paste the content you want to post in `tweetContent.txt`
+  
---
+++ Automating twitter/automattingTwitter.py
@@ -0,0 +1,36 @@
+import pyautogui
+import time
+
+time.sleep(4)
+
+# This only works on firfox with 100% view
+# and the screen size should be 1920 x 1080
+
+
+# This is for creating new tab (after some time i will make it a combination of hotkey instead of moving and clicking)
+# pyautogui.moveTo(1564,32,duration=2)
+# pyautogui.click()
+
+# This does above task of opening a new tab
+pyautogui.hotkey("ctrl", "t")
+
+# This code will take me to twitter
+pyautogui.moveTo(405,96,duration=1)
+pyautogui.click()
+with open('openingTabURL.txt', 'r') as x:
+    URL = x.read()
+pyautogui.typewrite(URL)
+pyautogui.moveTo(1419,97,duration=1)
+pyautogui.click()
+time.sleep(3)
+
+# This will write the post
+pyautogui.moveTo(577,339,duration=1)
+pyautogui.click()
+with open('tweetContent.txt', 'r') as x:
+    tweetContent = x.read()
+pyautogui.typewrite(tweetContent)
+
+# This will post on twitter
+pyautogui.moveTo(1257,540,duration=1)
+pyautogui.click()
